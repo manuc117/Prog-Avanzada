84238ce07ef1460f062722935c8ba28ce6355b38
manuc117 <manucalvo1107@gmail.com>
2023-01-23 21:11:42 -0300

    flitros nuevos

--- filtropasaaltos.cpp
+++ filtropasaaltos.cpp
@@ -11,45 +11,45 @@ Imagen FiltroPasaAltos::aplicarFiltro(Imagen &img)
     int auxRed, auxGreen, auxBlue;
     Pixel pixAux;
 
-    int nColumnas = imagen.getColumnas(), nFilas = imagen.getFilas();
+    int nColumnas = img.getColumnas(), nFilas = img.getFilas();
 
     for(int fila=0; fila<nFilas; fila++)
     {
         for(int columna=0; columna<nColumnas; columna ++)
         {
-            pixAux = imagen.getPixel(fila,columna);
+            pixAux = img.getPixel(fila,columna);
 
             auxRed = pixAux.getRed()*5;
             auxGreen = pixAux.getGreen()*5;
             auxBlue = pixAux.getBlue()*5;
 
-            if(imagen.estaEnLaImagen(fila-1,columna))
+            if(img.estaEnLaImagen(fila-1,columna))
             {
-                pixAux = imagen.getPixel(fila-1, columna);
+                pixAux = img.getPixel(fila-1, columna);
                 auxRed += pixAux.getRed()*(-1);
                 auxGreen += pixAux.getGreen()*(-1);
                 auxBlue += pixAux.getBlue()*(-1);
             }
 
-            if(imagen.estaEnLaImagen(fila,columna-1))
+            if(img.estaEnLaImagen(fila,columna-1))
             {
-                pixAux = imagen.getPixel(fila, columna-1);
+                pixAux = img.getPixel(fila, columna-1);
                 auxRed += pixAux.getRed()*(-1);
                 auxGreen += pixAux.getGreen()*(-1);
                 auxBlue += pixAux.getBlue()*(-1);
             }
 
-            if(imagen.estaEnLaImagen(fila,columna+1))
+            if(img.estaEnLaImagen(fila,columna+1))
             {
-                pixAux = imagen.getPixel(fila, columna+1);
+                pixAux = img.getPixel(fila, columna+1);
                 auxRed += pixAux.getRed()*(-1);
                 auxGreen += pixAux.getGreen()*(-1);
                 auxBlue += pixAux.getBlue()*(-1);
             }
 
-            if(imagen.estaEnLaImagen(fila+1,columna))
+            if(img.estaEnLaImagen(fila+1,columna))
             {
-                pixAux = imagen.getPixel(fila+1, columna);
+                pixAux = img.getPixel(fila+1, columna);
                 auxRed += pixAux.getRed()*(-1);
                 auxGreen += pixAux.getGreen()*(-1);
                 auxBlue += pixAux.getBlue()*(-1);
--- filtropasabajos.cpp
+++ filtropasabajos.cpp
@@ -11,77 +11,77 @@ Imagen FiltroPasaBajos::aplicarFiltro(Imagen &img)
     float auxRed, auxGreen, auxBlue;
     Pixel pixAux;
 
-    int nColumnas = imagen.getColumnas(), nFilas = imagen.getFilas();
+    int nColumnas = img.getColumnas(), nFilas = img.getFilas();
 
     for(int fila=0; fila<nFilas; fila++)
     {
         for(int columna=0; columna<nColumnas; columna ++)
         {
-            pixAux = imagen.getPixel(fila,columna);
+            pixAux = img.getPixel(fila,columna);
 
             auxRed = pixAux.getRed();
             auxGreen = pixAux.getGreen();
             auxBlue = pixAux.getBlue();
 
-            if(imagen.estaEnLaImagen(fila-1,columna-1))
+            if(img.estaEnLaImagen(fila-1,columna-1))
             {
-                pixAux = imagen.getPixel(fila-1, columna-1);
+                pixAux = img.getPixel(fila-1, columna-1);
                 auxRed += (float)pixAux.getRed();
                 auxGreen += (float)pixAux.getGreen();
                 auxBlue += (float)pixAux.getBlue();
             }
 
-            if(imagen.estaEnLaImagen(fila-1,columna))
+            if(img.estaEnLaImagen(fila-1,columna))
             {
-                pixAux = imagen.getPixel(fila-1, columna);
+                pixAux = img.getPixel(fila-1, columna);
                 auxRed += (float)pixAux.getRed();
                 auxGreen += (float)pixAux.getGreen();
                 auxBlue += (float)pixAux.getBlue();
             }
 
-            if(imagen.estaEnLaImagen(fila-1,columna+1))
+            if(img.estaEnLaImagen(fila-1,columna+1))
             {
-                pixAux = imagen.getPixel(fila-1, columna+1);
+                pixAux = img.getPixel(fila-1, columna+1);
                 auxRed += (float)pixAux.getRed();
                 auxGreen += (float)pixAux.getGreen();
                 auxBlue += (float)pixAux.getBlue();
             }
 
-            if(imagen.estaEnLaImagen(fila,columna-1))
+            if(img.estaEnLaImagen(fila,columna-1))
             {
-                pixAux = imagen.getPixel(fila, columna-1);
+                pixAux = img.getPixel(fila, columna-1);
                 auxRed += (float)pixAux.getRed();
                 auxGreen += (float)pixAux.getGreen();
                 auxBlue += (float)pixAux.getBlue();
             }
 
-            if(imagen.estaEnLaImagen(fila,columna+1))
+            if(img.estaEnLaImagen(fila,columna+1))
             {
-                pixAux = imagen.getPixel(fila, columna+1);
+                pixAux = img.getPixel(fila, columna+1);
                 auxRed += (float)pixAux.getRed();
                 auxGreen += (float)pixAux.getGreen();
                 auxBlue += (float)pixAux.getBlue();
             }
 
-            if(imagen.estaEnLaImagen(fila+1,columna-1))
+            if(img.estaEnLaImagen(fila+1,columna-1))
             {
-                pixAux = imagen.getPixel(fila+1, columna-1);
+                pixAux = img.getPixel(fila+1, columna-1);
                 auxRed += (float)pixAux.getRed();
                 auxGreen += (float)pixAux.getGreen();
                 auxBlue += (float)pixAux.getBlue();
             }
 
-            if(imagen.estaEnLaImagen(fila+1,columna))
+            if(img.estaEnLaImagen(fila+1,columna))
             {
-                pixAux = imagen.getPixel(fila+1, columna);
+                pixAux = img.getPixel(fila+1, columna);
                 auxRed += (float)pixAux.getRed();
                 auxGreen += (float)pixAux.getGreen();
                 auxBlue += (float)pixAux.getBlue();
             }
 
-            if(imagen.estaEnLaImagen(fila+1,columna+1))
+            if(img.estaEnLaImagen(fila+1,columna+1))
             {
-                pixAux = imagen.getPixel(fila+1, columna+1);
+                pixAux = img.getPixel(fila+1, columna+1);
                 auxRed += (float)pixAux.getRed();
                 auxGreen += (float)pixAux.getGreen();
                 auxBlue += (float)pixAux.getBlue();
